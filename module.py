8c791bbb5527f68809467c8f617676ecc362ef45
Just_a_hard <bonboncat.cycle@gmail.com>
2024-06-16 04:48:18 +0300

    File created

---
+++ module.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+CurrentWindow = None
+
+def destroy_prev_window():
+    return
+
+def show_main_window():
+    return
+
+def show_all():
+    return
+
+def import_data():
+    return
+
+def export_data():
+    return
