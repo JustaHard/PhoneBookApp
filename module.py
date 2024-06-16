3f235d74d66a5ab705b06634e62d4506b45538d0
Just_a_hard <bonboncat.cycle@gmail.com>
2024-06-16 04:54:25 +0300

    show_main_window func ready

--- module.py
+++ module.py
@@ -6,7 +6,35 @@ def destroy_prev_window():
     return
 
 def show_main_window():
-    return
+    global CurrentWindow
+
+    # Закрываем предыдущее окно
+    destroy_prev_window()
+
+    # Создаем главное окно приложения
+    root = tk.Tk()
+    root.title("Управление телефонным справочником")
+    root.geometry("400x300")
+    CurrentWindow = root
+
+    # Создаем кнопку для отображения всего справочника
+    ShowAllButton = tk.Button(root, text='Отобразить весь справочник', command=show_all)
+    ShowAllButton.pack(pady=10)
+
+    # Создаем кнопку для экспорта данных из справочника
+    ExportDataButton = tk.Button(root, text='Получить данные о существующем контакте', command=export_data)
+    ExportDataButton.pack(pady=10)
+
+    # Создаем кнопку для импорта данных в справочник
+    ImportDataButton = tk.Button(root, text='Добавить новый контакт', command=import_data)
+    ImportDataButton.pack(pady=10)
+
+    # Создаем кнопку завершения работы
+    CloseAppButton = tk.Button(root, text='Завершить работу', command=root.destroy)
+    CloseAppButton.pack(pady=10)
+
+    # Запуск главного цикла обработки событий
+    root.mainloop()
 
 def show_all():
     return
