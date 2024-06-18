aa77df7013dc154024610c887d7684865768478a
Just_a_hard <bonboncat.cycle@gmail.com>
2024-06-18 21:37:51 +0300

    working version 2.2

--- module.py
+++ module.py
@@ -4,6 +4,10 @@ from tkinter import ttk
 CurrentWindow = None
 
 def show_main_window():
+    '''
+    Главное окно, с которого начинается программа
+    '''
+
     # Создаем окно главного экрана
     root = create_window('Управление телефонным справочником', '400x300')
 
@@ -20,7 +24,17 @@ def show_main_window():
     root.mainloop()
 
 def show_all():
+    '''
+    Просмотр и изменение списка сохраненных контактов
+    '''
+
     def create_table(data):
+        """
+        Создаем таблицу с заданными данными
+
+        data(dict) - Словарь с данными, которые нужно отобразить в таблице
+        """
+
         global TableFrame
 
         # Создаем область, в которой располагается таблица
@@ -31,13 +45,16 @@ def show_all():
         cols = tuple(i for i in range(len(data)))
         cols_sizes = (30, 100, 100, 120, 130)
         Table = ttk.Treeview(TableFrame, columns=cols, show='headings', height=12)
-        
+
+        # Указываем заголовки таблицы
         for i in range(len(data)):
             Table.heading(cols[i], text=list(data.keys())[i])
 
+        # Указываем размеры столбцов таблицы
         for i in range(len(data)):
             Table.column(i, width=cols_sizes[i])
 
+        # Заносим данные в таблицу
         for i in range(len(list(data.values())[0])):
             for key in data.keys():
                 if key == '№':
@@ -45,32 +62,57 @@ def show_all():
                 Line.append(data[key][i])
             Table.insert('', tk.END, values=Line)
 
-        # Выводим таблицу в окно
+        # Выводим таблицу в окне
         Table.pack()
 
     def surname_sort():
+        """
+        Сортировка таблицы по фамилии контакта
+        """
+
+        # Получаем данные о сохраненных контактах
         PhoneBook = search_by_number()
+
+        # Сортируем данные по фамилии
         PhoneBook = dict_to_list(PhoneBook)
         PhoneBook = sorted(PhoneBook, key=lambda x: x[1])
         PhoneBook = list_to_dict(PhoneBook)
+
+        # Обновляем таблицу
         TableFrame.destroy()
         create_table(PhoneBook)
 
     def name_sort():
+        """
+        Сортировка таблицы по имени контакта
+        """
+
+        # Получаем данные о сохраненных контактах
         PhoneBook = search_by_number()
+
+        # Сортируем данные по имени
         PhoneBook = dict_to_list(PhoneBook)
         PhoneBook = sorted(PhoneBook, key=lambda x: x[2])
         PhoneBook = list_to_dict(PhoneBook)
+
+        # Обновляем таблицу
         TableFrame.destroy()
         create_table(PhoneBook)
 
     def search_by_number():
+        """
+        Фильтруем данные таблицы по введенному фрагменту номера телефона
+        """
+
+        # Получаем данные о сохраненных контактах
+        PhoneBook = get_phone_book()
+
         # Указываем переменную, в которую будем сохранять подходящие контакты
-        LinesWithNumber = {'№':[], 'Фамилия':[], 'Имя':[], 
-                           'Номер телефона':[], 'Комментарий':[]}
+        LinesWithNumber = dict()
+        for key in PhoneBook.keys():
+            LinesWithNumber[key] = []
         
         # Ищем контакты с указанным номером
-        PhoneBook = get_phone_book()
         for i in range(len(list(PhoneBook.values())[0])):
             if SearchByNumberEntry.get() in PhoneBook["Номер телефона"][i]:
                 for key in PhoneBook.keys():
@@ -81,14 +123,31 @@ def show_all():
         create_table(LinesWithNumber)
         return LinesWithNumber
 
-    def update_table():
+    def rebuild_table():
+        """
+        Возвращаем таблицу к исходному состоянию
+        """
+
+        # Получаем данные о сохраненных контактах
         PhoneBook = get_phone_book()
+
+        # Очищаем поле ввода для фильтрации таблицы по номеру
         SearchByNumberEntry.delete(0,tk.END)
+
+        # Обновляем таблицу
         TableFrame.destroy()
         create_table(PhoneBook)
 
     def change_data():
+        """
+        Внесение изменений в данные об указанном контакте
+        """
+
         def save_changes():
+            """
+            Сохранение внесенных изменений
+            """
+
             # Вносим изменения в список контактов
             for i in range(len(PhoneBook)):
                 PhoneBook[list(PhoneBook.keys())[i]][IndexToChange] = EntriesToChange[i].get()
@@ -98,6 +157,10 @@ def show_all():
             show_all()
 
         def delete_data():
+            """
+            Удаление указанного контакта из базы данных
+            """
+
             # Удаляем контакт из списка
             for i in range(len(PhoneBook)):
                 PhoneBook[list(PhoneBook.keys())[i]].pop(IndexToChange)
@@ -106,34 +169,58 @@ def show_all():
             update_data(PhoneBook)
             show_all()
         
+        # Проверяем что поле для указания индекса контакта не пустое
         if IndexToChangeEntry.get():
-            IndexToChange = int(IndexToChangeEntry.get())
-            PhoneBook = get_phone_book()
-            if IndexToChange in PhoneBook['№']:
-                # Создаем новое окно для редактирования контакта
-                ChangeDataWindow = create_window('Изменение данных о контакте', '500x500')
 
-                # Создаем кнопку для удаления контакта
-                create_button(ChangeDataWindow, 'Удалить контакт', delete_data, pady=10)
+            # Если введенную информацию нельзя представить в виде int, сообщаем об этом
+            try:
+                # Получаем данные о сохраненных контактах и индекс контакта, 
+                # который нужно изменить
+                IndexToChange = int(IndexToChangeEntry.get())
+                PhoneBook = get_phone_book()
 
-                # Даем пользователю возможность изменить данные о контакте
-                PhoneBook.pop('№')
-                EntriesToChange = []
-                for key in PhoneBook.keys():
-                    entry = create_entry(ChangeDataWindow, key, fpady=10)[1]
-                    entry.insert(0, PhoneBook[key][IndexToChange])
-                    EntriesToChange.append(entry)
+                # Проверяем, что контакт с указанным индексом существует
+                if IndexToChange in PhoneBook['№']:
+                    # Создаем новое окно для редактирования контакта
+                    ChangeDataWindow = create_window('Изменение данных о контакте', '500x500')
 
-                # Создаем кнопки для сохранения и отмены изменений
-                create_button(ChangeDataWindow, 'Сохранить', save_changes)
-                create_button(ChangeDataWindow, 'Отменить', show_all, pady=10)
-            
-            else:
-                IndexErrorWindow = create_window('Ошибка', '300x100')
-                IndexErrorLabel = tk.Label(IndexErrorWindow, 
-                                           text='Контакт с указанным индексом не существует')
-                IndexErrorLabel.pack(pady=10)
-                create_button(IndexErrorWindow, 'Назад', show_all, pady=10)
+                    # Создаем кнопку для удаления контакта
+                    create_button(ChangeDataWindow, 'Удалить контакт', delete_data, pady=10)
+
+                    # Даем пользователю возможность изменить данные о контакте
+                    PhoneBook.pop('№')
+                    EntriesToChange = []
+                    for key in PhoneBook.keys():
+                        entry = create_entry(ChangeDataWindow, key, fpady=10)[1]
+                        entry.insert(0, PhoneBook[key][IndexToChange])
+                        EntriesToChange.append(entry)
+
+                    # Создаем кнопки для сохранения и отмены изменений
+                    create_button(ChangeDataWindow, 'Сохранить', save_changes)
+                    create_button(ChangeDataWindow, 'Отменить', show_all, pady=10)
+                
+                else:
+                    # Создаем окно для вывода ошибки
+                    IndexErrorWindow = create_window('Ошибка', '300x100')
+
+                    # Выводим сообщение об ошибке
+                    IndexErrorLabel = tk.Label(IndexErrorWindow, 
+                                            text='Контакт с указанным индексом не существует')
+                    IndexErrorLabel.pack(pady=10)
+
+                    # Создаем кнопку для возврата к таблице с контактами
+                    create_button(IndexErrorWindow, 'Назад', show_all, pady=10)
+            except TypeError:
+                # Создаем окно для вывода ошибки
+                DataTypeErrorWindow = create_window('Ошибка', '300x100')
+
+                # Выводим сообщение об ошибке
+                DataTypeErrorLabel = tk.Label(DataTypeErrorWindow, 
+                                              text='Введенная информация не является индексом')
+                DataTypeErrorLabel.pack(pady=10)
+
+                # Создаем кнопку для возврата к таблице с контактами
+                create_button(DataTypeErrorWindow, 'Назад', show_all, pady=10)
             
     # Создаем окно для вывода списка сохраненных контактов
     ShowAllWindow = create_window('Список сохраненных контактов', '700x500')
@@ -141,6 +228,7 @@ def show_all():
     # Получаем список сохраненных контактов
     PhoneBook = get_phone_book()
 
+    # Проверяем, есть ли сохраненные контакты
     if PhoneBook['№']:
         # Создаем окно для взаимодействия со списком сохраненных контактов
         InteractionFrame = tk.Frame(ShowAllWindow)
@@ -150,7 +238,7 @@ def show_all():
         create_button(InteractionFrame, 'Сортировать по фамилии', 
                       surname_sort, padx=10, side='left')
 
-        # Доавляем кнопку для сортировки по имени
+        # Добавляем кнопку для сортировки по имени
         create_button(InteractionFrame, 'Сортировать по имени', 
                       name_sort, padx=10, side='left')
 
@@ -169,7 +257,7 @@ def show_all():
 
         # Создаем кнопку для возврата таблицы в исходное состояние
         create_button(ShowAllWindow, 'Вернуть таблицу к исходному состоянию', 
-                      update_table, pady=10, side='bottom')
+                      rebuild_table, pady=10, side='bottom')
 
         # Создаем поле для выбора контакта для редактирования
         IndexToChangeFrame, IndexToChangeEntry = create_entry(ShowAllWindow, 
@@ -179,14 +267,28 @@ def show_all():
     else:
         # Если контактов не обнаружено, сообщаем об этом
         NoContactsLabel = tk.Label(ShowAllWindow, text='Нет сохраненных контактов')
-        NoContactsLabel.pack(side='top')
+        NoContactsLabel.pack(side='top', pady=20)
+
+        # Создаем кнопку для возврата на главный экран
+        create_button(ShowAllWindow, 'Назад', show_main_window, pady=10, side='bottom')
 
 def import_data():
+    """
+    Сохранение нового контакта
+    """
+
     def save_data():
+        """
+        Внесение нового контакта в базу данных
+        """
+
+        # Получаем данные о ранее сохраненных контактах и приводим их в удобный вид
         PhoneBook = get_phone_book()
         PhoneBook.pop('№')
         comments = PhoneBook.pop('Комментарий')
         PhoneBook = dict_to_list(PhoneBook)
+
+        # Получаем данные о контакте, который нужно сохранить
         DataToSave = [data.get() for data in Entries]
 
         # Заносим новый контакт в список, если до этого его там не было
@@ -202,15 +304,21 @@ def import_data():
             import_data()
 
         else:
+            # Создаем окно, чтобы сообщить пользователю, что контакт уже существует
             ContactAlreadyExistsWindow = create_window('Контакт уже существует', '300x100')
+
+            # Выводим сообщение
             ContactAlreadyExistsLabel = tk.Label(ContactAlreadyExistsWindow, 
                                                 text='Данный контакт уже существует')
             ContactAlreadyExistsLabel.pack(pady=10)
+
+            # Создаем кнопку для возврата к окну введения данных о новом контакте
             create_button(ContactAlreadyExistsWindow, 'Назад', import_data, pady=10)
 
     # Создаем окно для создания нового контакта
     ImportDataWindow = create_window('Введите данные о новом контакте', '400x300')
     
+    # Создаем поля ввода для указания данных о новом контакте
     Entries = []
     for i in ['Фамилия', 'Имя', 'Номер телефона', 'Комментарий']:
         entry = create_entry(ImportDataWindow, i, fpady=10, fpadx=20, lside='left', 
@@ -223,9 +331,14 @@ def import_data():
     # Создаем кнопку возврата в главное меню
     create_button(ImportDataWindow, 'Назад', show_main_window, pady=10)
 
-def get_phone_book():
+def get_phone_book(filepath='Saved_Data/Phone_book.txt'):
+    """
+    Получение данных об уже сохраненных контактах
+
+    filepath(Path) - Путь к файлу, из которого нужно получить данные
+    """
+    
     data = []
-    filepath = 'Saved_Data/Phone_book.txt'
 
     # Получаем данные о существующих контактах
     with open(filepath, 'r', encoding='utf-8') as phb:
@@ -248,14 +361,38 @@ def get_phone_book():
         
     return PhoneBook
 
+def update_data(data, filepath='Saved_Data/Phone_book.txt'):
+    """
+    Обновляем список сохраненных контактов
+    
+    data - Данные, которые мы сохраняем вместо предыдущих
+    filepath(Path) - Путь к файлу, в который нужно загрузить данные
+    """
+
+    # Если данные записаны в виде словаря, переводим их в вид массива
+    if type(data) == dict:
+        data = dict_to_list(data)
+
+    with open(filepath, 'w', encoding='utf-8') as phb:
+            for line in data:
+                s = ','.join(str(data) for data in line)
+                phb.write(f'{s}\n')
+
 def create_window(title, geometry):
+    """
+    Создаем новое окно и удаляем предыдущее
+
+    title(str) - Заголовок окна
+    geometry('WIDTHxHEIGHT') - Размер окна в пикселях
+    """
+
     global CurrentWindow
 
-    def destroy_prev_window():
-        if CurrentWindow is not None:
-            CurrentWindow.destroy()
+    # Удаляем предыдущее окно
+    if CurrentWindow is not None:
+        CurrentWindow.destroy()
 
-    destroy_prev_window()
+    # Создаем новое окно
     window = tk.Tk()
     window.title(title)
     window.geometry(geometry)
@@ -263,13 +400,37 @@ def create_window(title, geometry):
     return window
 
 def create_button(area, text, func, pady=None, padx=None, side=None):
+    """
+    Создаем кнопку
+    area(window or frame) - Область, в которой будет располагаться кнопка
+    text(str) - Текст на кнопке
+    func - Функция, которая будет выполняться при нажатии на кнопку
+    pady(int) - Вертикальный отступ кнопки
+    padx(int) - Горизонтальный отступ кнопки
+    side['top', 'bottom', 'left', 'right'] - Сторона area, к которой будет 
+        стремиться кнопка
+    """
+
     Button = tk.Button(area, text=text, command=func)
     Button.pack(pady=pady, padx=padx, side=side)
 
-    return Button
-
 def create_entry(area, text, width=30, fside=None, lside=None, eside=None, 
                  fpady=None, fpadx=None):
+    """
+    Создаем поле ввода
+    area(window or frame) - Область, в которой будет располагаться поле
+    text(str) - Текст рядом с полем ввода
+    width(int) - Ширина поля ввода
+    fside['top', 'bottom', 'left', 'right'] - Сторона area, к которой будет 
+        стремиться созданная область
+    lside['top', 'bottom', 'left', 'right'] - Сторона frame, к которой будет 
+        стремиться текст рядом с полем ввода
+    fside['top', 'bottom', 'left', 'right'] - Сторона frame, к которой будет 
+        стремиться поле ввода
+    fpady(int) - Вертикальный отступ созданной области
+    fpadx(int) - Горизонтальный отступ созданной области
+    """
+
     # Создаем область, в которой будут располагаться поле ввода и его пояснение
     Frame = tk.Frame(area)
     Frame.pack(side=fside, padx=fpadx, pady=fpady)
@@ -285,6 +446,12 @@ def create_entry(area, text, width=30, fside=None, lside=None, eside=None,
     return Frame, Entry
 
 def dict_to_list(Dict):
+    """
+    Из словаря с данными создаем массив с данными
+    
+    Dict - Словарь, из которого нужно сделать массив
+    """
+
     List = []
     for i in range(len(list(Dict.values())[0])):
         List.append(list())
@@ -294,20 +461,15 @@ def dict_to_list(Dict):
     return List
 
 def list_to_dict(List):
+    """
+    Из массива с данными создаем словарь с данными
+    
+    List - Массив, из которого нужно сделать словарь
+    """
+
     Dict = {'№':[], 'Фамилия':[], 'Имя':[], 'Номер телефона':[], 'Комментарий':[]}
     for i in range(len(List)):
         for j in range(len(List[i])):
             Dict[list(Dict.keys())[j]].append(List[i][j])
 
     return Dict
-
-def update_data(data):
-    filepath = 'Saved_Data/Phone_book.txt'
-
-    if type(data) == dict:
-        data = dict_to_list(data)
-
-    with open(filepath, 'w', encoding='utf-8') as phb:
-            for line in data:
-                s = ','.join(str(data) for data in line)
-                phb.write(f'{s}\n')
